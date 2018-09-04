8b4c15db9d756a3f815f32d463c543e6bb1a7ad9
BGoldburd <benjamingoldburd@gmail.com>
2018-09-03 21:44:54 -0400

    using jQuery ui

--- 68/mrPotatoHead.js
+++ 68/mrPotatoHead.js
@@ -11,5 +11,28 @@
 
     $('#startOver').click(() => {
         $('.piece, .body').css({top: 0, left: 0});
+        localStorage.clear(0);
+        zIndex = 0;
     });
+
+    $('.piece, .body').mouseup(function () {
+        console.log('top: ' + $(this).css('top'));
+        console.log('left: ' + $(this).css('left'));
+        console.log('zindex: ' + $(this).css('z-index'));
+        console.log(this.id);
+        localStorage.setItem(this.id, JSON.stringify({
+            top: $(this).css('top'), 
+            left: $(this).css('left'), 
+            zIndex: $(this).css('z-index')
+        }));
+        localStorage.setItem('zIndex', $(this).css('z-index'));
+    });
+
+    let allPieces = Object.keys(localStorage);
+    zIndex = localStorage.getItem('zIndex');
+
+    allPieces.forEach(piece => {
+        $(`#${piece}`).css(JSON.parse(localStorage.getItem(piece)));
+    });
+    
 }());
