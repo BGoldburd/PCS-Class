52a24a19d3317af93e347a694a6078c5175268a7
BGoldburd <benjamingoldburd@gmail.com>
2018-11-17 23:42:20 -0500

    flux

---
+++ 81/usingFlux/src/data/TodoStore.js
@@ -0,0 +1,46 @@
+import Immutable from 'immutable';
+import { ReduceStore } from 'flux/utils';
+import TodoActionTypes from './TodoActionTypes';
+import TodoDispatcher from './TodoDispatcher';
+import Todo from './Todo.js';
+
+class TodoStore extends ReduceStore {
+    constructor() {
+        super(TodoDispatcher);
+        this.nextId = 1;
+    }
+
+    getInitialState() {
+        return Immutable.OrderedMap();
+    }
+
+    reduce(state, action) {
+        switch (action.type) {
+            case TodoActionTypes.ADD_TODO:
+                // Don't add todos with no text.
+                if (!action.text) {
+                    return state;
+                }
+                const id = this.nextId++;
+                return state.set(id, new Todo({
+                    id,
+                    text: action.text,
+                    complete: false
+                }));
+
+            case TodoActionTypes.DELETE_TODO:
+                return state.delete(action.id);
+        
+            case TodoActionTypes.TOGGLE_TODO:
+                return state.update(
+                  action.id,
+                  todo => todo.set('complete', !todo.complete),
+                );
+
+            default:
+                return state;
+        }
+    }
+}
+
+export default new TodoStore();
